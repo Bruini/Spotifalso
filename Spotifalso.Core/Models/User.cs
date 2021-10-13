087740c31c4b4774dbff67b485e6d9be938f8c8b
Pedro Bruini <pedro.bruini@hotmail.com>
2021-10-13 19:48:35 -0300

    add user model

---
+++ Spotifalso.Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Spotifalso.Core.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string ProfilePhotoId { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Nickname { get; set; }
+        public string Bio { get; set; }
+    }
+}
