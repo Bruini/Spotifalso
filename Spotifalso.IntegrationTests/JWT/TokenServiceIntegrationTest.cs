e8f7fe4e58cf3fbf3e28787555ed66827b49d3c8
Pedro Bruini <pedro.bruini@hotmail.com>
2021-10-28 00:12:01 -0300

    add jwt token integration service

---
+++ Spotifalso.IntegrationTests/JWT/TokenServiceIntegrationTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Spotifalso.Aplication.Interfaces.Infrastructure;
+using Spotifalso.Core.Enums;
+using Spotifalso.Core.Models;
+using Spotifalso.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace Spotifalso.IntegrationTests.JWT
+{
+    public class TokenServiceIntegrationTest : IClassFixture<BaseServicesTestFixture>
+    {
+        private readonly BaseServicesTestFixture _fixture;
+        private readonly ITokenService _tokenService;
+        public TokenServiceIntegrationTest(BaseServicesTestFixture fixture)
+        {
+            _fixture = fixture;
+            _tokenService = _fixture.ServiceProvider.GetRequiredService<ITokenService>();
+        }
+
+        [Theory]
+        [InlineData(Roles.Admin)]
+        [InlineData(Roles.Subscriber)]
+        public void Should_GenerateToken(Roles role)
+        {
+            var user = new User(string.Empty, "abc001", role, "Pedro", string.Empty);
+            var token = this._tokenService.GenerateToken(user);
+
+            Assert.NotNull(token);
+            Assert.NotEqual(string.Empty, token);
+        }
+    }
+}
